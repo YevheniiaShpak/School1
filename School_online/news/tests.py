8850d797e0fa1404d4669b6775eb1daf900210cc
eva <shpakes86@gmail.com>
2024-08-15 15:27:02 +0300

    test and logo

--- School_online/news/tests.py
+++ School_online/news/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Articles
 
-# Create your tests here.
+class ArticlesModelTest(TestCase):
+
+    def setUp(self):
+        self.article = Articles.objects.create(
+            title="Тестовая статья",
+            anons="Краткий анонс",
+            full_text="Полный текст",
+            date="2024-08-15"
+        )
+
+    def test_article_creation(self):
+        self.assertIsInstance(self.article, Articles)
+        self.assertEqual(self.article.title, "Тестовая статья")
+
+
