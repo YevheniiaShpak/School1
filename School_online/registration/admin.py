a3162fc64a21fc736716342237f53bb5be95312f
eva <shpakes86@gmail.com>
2024-08-15 19:27:24 +0300

    em

--- School_online/registration/admin.py
+++ School_online/registration/admin.py
@@ -3,13 +3,13 @@ from .models import (
     Student,
     Teacher,
     Parent,
-    Enrollment
+
 )
 
 admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(Parent)
-admin.site.register(Enrollment)
+
 
 
 
